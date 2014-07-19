e12c0996fa79ac64e517e9e20e33884ebbd12b4e
Pawel Nowosielski <nowosielski@gmail.com>
2014-07-19 15:44:27 +0200

    Added Gulp.js build system

---
+++ gulpfile.coffee
@@ -0,0 +1,8 @@
+﻿gulp = require 'gulp' 
+mocha = require 'gulp-mocha'
+
+gulp.task 'test', ->
+    gulp.src 'test/*.coffee', read: false
+        .pipe(mocha reporter: 'nyan')
+
+gulp.task 'default', ['test']
---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+﻿// @src:http://pem-musing.blogspot.com/2014/02/a-gulp-of-coffee-your-gulpfile-in.html
+
+// Note the new way of requesting CoffeeScript since 1.7.x
+require('coffee-script/register');
+// This bootstraps your Gulp's main file
+require('./Gulpfile.coffee');
